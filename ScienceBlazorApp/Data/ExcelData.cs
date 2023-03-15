ce6f065d27b7a4b90eae0eeca88a175120e191b3
kennyNc <93539761+kennyNc@users.noreply.github.com>
2023-03-15 12:42:55 -0600

    Delete ScienceBlazorApp directory

--- ScienceBlazorApp/Data/ExcelData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ScienceBlazorApp.Data;
-
-public struct ExcelData
-{
-    public string Pm { get; set; }
-    public string Pb { get; set; }
-    public string Qm { get; set; }
-    public string Qb { get; set; }    
-}
